fb312be5a222f2b78d6559cbe3db8cde4e5c0ebd
Alexandr Yakubenko <alexandr.yakubenko@gmail.com>
2013-10-13 21:31:09 +0300

    added capistrano and deploy scripts

---
+++ config/unicorn.rb
@@ -0,0 +1,14 @@
+root = "/root/apps/wisf/current"
+
+working_directory root
+
+pid "#{ root }/tmp/pids/unicorn.pid"
+
+stderr_path "#{ root }/log/unicorn_err.log"
+stdout_path "#{ root }/log/unicorn.log"
+
+listen '/tmp/unicorn.wisf.sock'
+
+worker_processes 2
+
+timeout 30
