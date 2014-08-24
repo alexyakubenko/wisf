d6e8ea30124a3fa99ea3c06d5f4a4367f17ba74b
Alexandr Yakubenko <alexandr.yakubenko@gmail.com>
2014-08-24 11:37:33 +0300

    do not allow  english comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ActiveRecord::Base
   validates_presence_of :body
+  validate :non_english?
+
   belongs_to :story
+
+  def non_english?
+    self.body.downcase.scan(/[a-z]/).size < self.body.size
+  end
 end
