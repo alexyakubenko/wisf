d1cf6645e4caf264e3199eeb3138ca44192b893c
Alexandr Yakubenko <alexandr.yakubenko@gmail.com>
2014-08-24 11:41:13 +0300

    deny english stories

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,9 @@ class Story < ActiveRecord::Base
   validates_presence_of :content
   validates_length_of :content, :minimum => 10
   has_many :comments, :order => 'created_at ASC', :dependent => :delete_all
+  validate :non_english?
+
+  def non_english?
+    self.content.downcase.scan(/[a-z]/).size < self.content.size
+  end
 end
